f4365464c195bc3df0f33346ddb7c9c6908dcac3
Jeremy Sklarsky <jsklarsky@mediamath.com>
2016-03-10 23:51:03 -0500

    net http

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,16 +6,21 @@ class SessionsController < ApplicationController
   def create
     user_name = params[:user][:user_name]
     password = params[:user][:password]
-    @response = Unirest.post "https://t1qa13.mediamath.com/api/v2.0/login",
-    headers:{ "Accept" => "application/json" },
-    parameters:{
-      "api_key" => ENV['api_key'],
-      "user" => user_name,
-      "password" => password
-    }
-        
-    if @response.code == 200   
-      @user = User.find_or_create_by(:name => params[:user][:user_name])
+    # @response = Unirest.post "https://t1qa13.mediamath.com/api/v2.0/login",
+    # headers:{ "Accept" => "application/json" },
+    # parameters:{
+    #   "api_key" => ENV['api_key'],
+    #   "user" => user_name,
+    #   "password" => password
+    # }
+
+    params = {'api_key' => ENV['api_key'], 'user' => user_name, 'password' => password}
+    url = URI.parse('https://t1qa13.mediamath.com/api/v2.0/login')
+    resp, data = Net::HTTP.post_form(url, params)
+
+    # if @response.code == 200   
+    if resp.body.include?("ok")
+      @user = User.find_or_create_by(:name => user_name)
       login(@user)
       redirect_to root_path
     else
--- config/environment.rb
+++ config/environment.rb
@@ -2,4 +2,6 @@
 require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
+require 'open-uri'
+require 'net/http'
 Rails.application.initialize!
