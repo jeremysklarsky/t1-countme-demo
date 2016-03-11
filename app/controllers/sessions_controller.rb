bfa5d085f83cf41185fa523ea27cc9ef89b7869e
Jeremy Sklarsky <jsklarsky@mediamath.com>
2016-03-11 07:32:29 -0500

    fix fake login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,24 +6,9 @@ class SessionsController < ApplicationController
   def create
     user_name = params[:user][:user_name]
     password = params[:user][:password]
-    # @response = Unirest.post "https://t1qa13.mediamath.com/api/v2.0/login",
-    # parameters:{
-    #   "api_key" => ENV['api_key'],
-    #   "user" => user_name,
-    #   "password" => password
-    # }
-    
-    # xml  = Nokogiri::XML(@response.body)
-
-    # if xml.children[0].children[-2].attributes["code"].value == "ok"
-      @user = User.find_or_create_by(:name => params[:user][:user_name])
-      login(@user)
-      redirect_to root_path
-    # else
-    #   flash.now[:notice] = "User name or password is not valid."
-    #   render 'new'
-    # end
-
+    @user = User.find_or_create_by(:name => user_name)
+    login(@user)
+    redirect_to root_path
   end
 
   def destroy
