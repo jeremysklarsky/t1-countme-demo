cde979255c8e72d6e09edc118d8b9e372508bd4c
Jeremy Sklarsky <jsklarsky@mediamath.com>
2016-03-11 08:43:03 -0500

    params

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -10,9 +10,9 @@ class UserController < ApplicationController
   end
 
   def hipchat
-    message = params[:item][:message][:message].split(" ")
+    message = hip_params[:item][:message][:message].split(" ")
     if message.first == '/checkin'
-      @hipchat_id = params["item"]["message"]["from"]["id"]
+      @hipchat_id = hip_params["item"]["message"]["from"]["id"]
       @username = finduser(@hipchat_id)
       @meeting = Meeting.where(epic: message[1], meeting_type: 'T1 Breakfast').first
       @user = User.find_or_create_by(name: @username)
@@ -29,6 +29,11 @@ class UserController < ApplicationController
     # url = "https://api.hipchat.com/v2/user/?auth_token=YOUR_TOKEN"
     @response = Unirest.get "https://api.hipchat.com/v2/user/#{id}?auth_token=#{ENV['hipchat_token']}"
     @response.body["email"].split("@").first
+  end
+
+private
+  def hip_params
+    params.require(:item).permit!
   end
 
 end
