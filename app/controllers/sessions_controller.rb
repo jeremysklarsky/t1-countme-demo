0bef07b6a85233951a66eb13003a52156626ed58
Jeremy Sklarsky <jsklarsky@mediamath.com>
2016-03-11 07:06:33 -0500

    not working

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,17 +4,17 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user_name = params[:user][:user_name]
-    password = params[:user][:password]
-    # @response = Unirest.post "https://t1qa13.mediamath.com/api/v2.0/login",
-    # headers:{ "Accept" => "application/json" },
-    # parameters:{
-    #   "api_key" => ENV['api_key'],
-    #   "user" => user_name,
-    #   "password" => password
-    # }
+    @user_name = params[:user][:user_name]
+    @password = params[:user][:password]
+    @response = Unirest.post "https://t1qa13.mediamath.com/api/v2.0/login",
+    headers:{ "Accept" => "application/json" },
+    parameters:{
+      "api_key" => ENV['api_key'],
+      "user" => user_name,
+      "password" => password
+    }
 
-    params = {'api_key' => ENV['api_key'], 'user' => user_name, 'password' => password}
+    params = {'api_key' => ENV['api_key'], 'user' => @user_name, 'password' => @password}
     url = URI.parse('https://t1qa13.mediamath.com/api/v2.0/login')
     resp, data = Net::HTTP.post_form(url, params)
 
@@ -29,7 +29,9 @@ class SessionsController < ApplicationController
       flash.now[:notice] = "User name or password is not valid."
       render 'new'
     end
-    
+    #  respond_to do |format|
+    #   format.js  # index.html.erb
+    # end 
 
    # xml  = Nokogiri::XML(@response.body)
 
