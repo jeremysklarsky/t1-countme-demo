457d3db0917c146382090c402ba291a2d72e83a0
Jeremy Sklarsky <jsklarsky@mediamath.com>
2016-03-11 08:29:33 -0500

    null session

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,36 +5,36 @@ class SessionsController < ApplicationController
 
   def create
 # <<<<<<< HEAD
-#     @user_name = params[:user][:user_name]
-#     @password = params[:user][:password]
-#     @response = Unirest.post "https://t1qa13.mediamath.com/api/v2.0/login",
-#     headers:{ "Accept" => "application/json" },
-#     parameters:{
-#       "api_key" => ENV['api_key'],
-#       "user" => user_name,
-#       "password" => password
-#     }
+    @user_name = params["user"]["user_name"]
+    @password = params["user"]["password"]
+    # @response = Unirest.post "https://t1qa13.mediamath.com/api/v2.0/login",
+    # headers:{ "Accept" => "application/json" },
+    # parameters:{
+    #   "api_key" => ENV['api_key'],
+    #   "user" => @user_name,
+    #   "password" => @password
+    # }
 
-#     params = {'api_key' => ENV['api_key'], 'user' => @user_name, 'password' => @password}
+#     new_params = {'api_key' => ENV['api_key'], 'user' => @user_name, 'password' => @password}
 #     url = URI.parse('https://t1qa13.mediamath.com/api/v2.0/login')
-#     resp, data = Net::HTTP.post_form(url, params)
+#     resp, data = Net::HTTP.post_form(url, new_params)
 
-#     puts resp.body
-
+#     # puts resp.body
+#     binding.pry
 #     # if @response.code == 200   
 #     if resp.body.include?("ok")
-#       @user = User.find_or_create_by(:name => user_name)
+#       @user = User.find_or_create_by(:name => @user_name)
 #       login(@user)
 #       redirect_to root_path
 #     else
 #       flash.now[:notice] = "User name or password is not valid."
 #       render 'new'
 #     end
-#     #  respond_to do |format|
-#     #   format.js  # index.html.erb
+#      # respond_to do |format|
+#      #  format.js  # index.html.erb
 #     # end 
 
-#    # xml  = Nokogiri::XML(@response.body)
+# #    # xml  = Nokogiri::XML(@response.body)
 
 
 # =======
